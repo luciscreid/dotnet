4338b1336499048fc7526ccfba780d75b21980dd
luciscreid <lucineicristian@gmail.com>
2023-04-12 10:10:41 -0300

    arquivo

---
+++ console/ListaLigada.cs
@@ -0,0 +1,39 @@
+﻿namespace console
+{
+    class ListaLigada
+    {
+        private Nodo primeiro;
+
+        public bool ehvazia() {
+            bool ehvazia = false;
+            if(primeiro == null){
+                ehvazia = true;
+            }
+            return ehvazia;
+        }
+
+
+        public void add(string palavra){
+            for(int i = 0; lista[i] != null; i++){
+                lista[i] = new Nodo(palavra);
+            }
+        }
+        
+        public int posicao(string palavra) {
+            
+            return 0;
+        }
+        
+        
+    }
+
+    class Nodo
+    {
+        public Nodo proximo { get; set; }
+        public string info { get; set; }
+        public Nodo(string info)
+        {
+            this.info = info;
+        }
+    }
+}
--- console/Program.cs
+++ console/Program.cs
@@ -17,38 +17,4 @@ namespace console
             Console.WriteLine("Deu Bom!");
 	    }
     }
-    class Nodo {
-        public Nodo proximo {get; set;}
-        public string info{get; set;}
-        public Nodo(string info){
-            this.info = info;
-        }
-    }
-
-    class ListaLigada
-    {
-        private Nodo primeiro;
-
-        public bool ehvazia() {
-            bool ehvazia = false;
-            if(primeiro == null){
-                ehvazia = true;
-            }
-            return ehvazia;
-        }
-
-
-        public void add(string palavra){
-            for(int i = 0; lista[i] != null; i++){
-                lista[i] = new Nodo(palavra);
-            }
-        }
-        
-        public int posicao(string palavra) {
-            
-            return 0;
-        }
-        
-        
-    }
 }
