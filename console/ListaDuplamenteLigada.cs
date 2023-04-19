0a321e09c428ed87a7863ac654ea49affab51036
luciscreid <lucineicristian@gmail.com>
2023-04-19 09:42:04 -0300

    lista duplamente ligada pronta

--- console/ListaDuplamenteLigada.cs
+++ console/ListaDuplamenteLigada.cs
@@ -12,24 +12,51 @@ namespace console
 
         public void add(string palavra)
         {
-            if(primeiro == null)
+            if (primeiro == null)
             {
                 primeiro = new Nodo(palavra);
             }
             else
             {
-                var nodoAtual = primeiro;
-                while(nodoAtual.proximo != null)
+                Nodo nodoAtual = primeiro;
+                while (nodoAtual.proximo != null)
                 {
                     nodoAtual = nodoAtual.proximo;
                 }
-                nodoAtual.proximo = new Nodo(palavra);
+
+                Nodo novo = new Nodo(palavra);
+                nodoAtual.proximo = novo;
+                novo.anterior = nodoAtual;
             }
         }
 
         public void add(string palavra, int posicao)
         {
-            throw new NotImplementedException();
+            Nodo novo = new Nodo(palavra);
+            if (primeiro == null)
+            {
+                primeiro = novo;
+            }
+            else
+            {
+                var nodoAtual = primeiro;
+                int i = 0;
+                while (i < posicao-1)
+                {
+                    nodoAtual = nodoAtual.proximo;
+                    i++;
+                }
+                var temp = nodoAtual;
+                nodoAtual.proximo = novo;
+                nodoAtual.proximo.anterior = temp;
+
+                //nodoAtual.anterior = nodoAtual.proximo.anterior;
+                           
+                //var temp = nodoAtual;
+                //nodoAtual = novo;
+                //nodoAtual.anterior.proximo = nodoAtual;
+                //nodoAtual.proximo = temp;
+            }
         }
 
         public bool ehvazia()
@@ -44,17 +71,79 @@ namespace console
 
         public string Get(int posicao)
         {
-            throw new NotImplementedException();
+            var Nodo = GetNodo(posicao);
+
+            return Nodo?.info;
+
+            //if (Nodo == null)
+            //{
+            //    return null;
+            //}
+            //else return Nodo.info;
+        }
+
+        private Nodo GetNodo(int posicao)
+        {
+            Nodo nodoAtual = primeiro;
+            if (posicao > tamanho())
+            {
+                return null;
+            }
+            else if (nodoAtual != null)
+            {
+                for (int i = 0; i < posicao; i++)
+                {
+                    nodoAtual = nodoAtual.proximo;
+                }
+                return nodoAtual;
+            }
+            return null;
         }
 
         public int posicao(string palavra)
         {
-            throw new NotImplementedException();
+            int i = 0;
+            Nodo nodoAtual = primeiro;
+            while (nodoAtual != null)
+            {
+                if (nodoAtual.info == palavra)
+                {
+                    return i;
+                }
+                nodoAtual = nodoAtual.proximo;
+                i++;
+            }
+            return -1;
         }
 
         public void remove(string palavra)
         {
-            throw new NotImplementedException();
+            if (primeiro != null)
+            {
+                if (primeiro.info == palavra)
+                {
+                    primeiro = primeiro.proximo;
+                    return;
+                }
+            }
+
+            Nodo nodoAtual = primeiro;
+            if (nodoAtual != null)
+            {
+                while (nodoAtual.proximo != null)
+                {
+                    if (nodoAtual.info == palavra)
+                    {
+                        break;
+                    }
+
+                    nodoAtual = nodoAtual.proximo;
+                }
+                var temp = nodoAtual.proximo;
+                nodoAtual = nodoAtual.anterior;
+                nodoAtual.proximo = temp;
+
+            }
         }
 
         public int tamanho()
